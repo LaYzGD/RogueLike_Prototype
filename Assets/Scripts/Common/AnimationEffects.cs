4d0d61f86d8f44aac7cd0a72f321ae8de636c0ed
LaYzGD <sollynobiuz@gmail.com>
2024-05-28 23:11:01 +0200

    BossAnimations

--- Assets/Scripts/Common/AnimationEffects.cs
+++ Assets/Scripts/Common/AnimationEffects.cs
@@ -6,6 +6,20 @@ public class AnimationEffects : MonoBehaviour
     [SerializeField] private AudioSource _musicSource;
     [SerializeField] private float _minimumPitch = 0.7f;
     [SerializeField] private AudioClip[] _randomSounds;
+    [SerializeField] private ParticleSystem[] _animationParticles;
+    [SerializeField] private Transform _particleSpawnPosition;
+
+    public void PlayParticles(int index)
+    {
+        _animationParticles[index].transform.position = _particleSpawnPosition.position;
+        _animationParticles[index].Play();
+    }
+
+    public void StopParticles(int index)
+    {
+        _animationParticles[index].Stop();
+    }
+
     public void PlaySound(AudioClip clip)
     {
         _audioSource.PlayOneShot(clip);
